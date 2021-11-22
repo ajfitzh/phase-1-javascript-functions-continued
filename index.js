74ab308620725a3a7b40b83d99253749601c5ab6
Austin <austin.j.fitzhugh@gmail.com>
2021-11-22 17:30:12 -0500

    commit tree ff5b64d8c92bde7d0b16de57c26955915f295e32

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function saturdayFun(activity="roller-skate") {
+function saturdayFun (activity = "roller-skate"){
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(activity="go to the office") {
+let mondayWork = function(activity="go to the office"){
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function wrapAdjective(string = "*"){
+  return function(parameter = 'special'){
+    return `You are ${string}${parameter}${string}!`
   }
 }
